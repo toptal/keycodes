4119abbbda969df2a0a41cd2029969129d38184f
e_palm <epalmans@gmail.com>
2018-10-10 17:19:40 +0200

    set focus on hidden input when user taps somewhere on the page --> opens up keyboard on mobile

--- scripts.js
+++ scripts.js
@@ -234,6 +234,10 @@ body.onkeydown = function(e) {
   //   `huh? Let me know what browser and key this was. <a href="https://github.com/wesbos/keycodes/issues/new?title=Missing keycode ${e.keyCode}&body=Tell me what key it was or even better, submit a Pull request!">Submit to Github</a>`;
 };
 
+body.addEventListener('touchend', function() {
+  document.getElementById('hidden_input').focus();
+}, true);
+
 (function(i, s, o, g, r, a, m) {
   i.GoogleAnalyticsObject = r;
   (i[r] =
