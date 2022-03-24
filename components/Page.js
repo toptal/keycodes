fee31e045fbb185a93d105e58faf1b56ffde5c9d
Wes Bos <wesbos@gmail.com>
2022-03-24 14:04:37 -0400

    nuke SW

--- components/Page.js
+++ components/Page.js
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 import { useKeyWatcher } from '../lib/useKeyCode';
-import { useNukeSW } from '../lib/useServiceWorker';
 import EventCollection from './EventCollection';
 
 export default function Page({ children }) {
   useKeyWatcher();
-  useNukeSW();
   return (
     <div>
       <EventCollection />
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,22 @@
-navigator.serviceWorker.getRegistrations().then((registrations) => {
-  for (const registration of registrations) {
-    console.log('unregistering service worker', registration);
-    registration.unregister();
-  }
+// navigator.serviceWorker.getRegistrations().then((registrations) => {
+//   for (const registration of registrations) {
+//     console.log('unregistering service worker', registration);
+//     registration.unregister();
+//   }
+// });
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Installing Service Worker ...', e);
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate');
+  self.registration
+    .unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      console.log(clients);
+      clients.forEach((client) => client.navigate(client.url));
+    });
 });
