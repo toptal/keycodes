05103d6fab67f27af4077467b05ca3ee981352ec
Wes Bos <wesbos@gmail.com>
2022-03-28 16:58:07 -0400

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,23 @@ export default function MyApp({ Component, pageProps }) {
           name="twitter:image"
           content="https://p198.p4.n0.cdn.getcloudapp.com/items/5zuL1xLY/0e0a6eb0-d30c-4e83-9562-1c09448fe3c0.png"
         />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-50371747-1"
+        />
+        <script
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-50371747-1');
+          `,
+          }}
+        />
       </Head>
       <Page>
         <Component {...pageProps} />
