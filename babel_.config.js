a5686a5eddff87f72d8525fd3d5e6651bbc97e05
Yusuf Yilmaz <yusuf.yilmaz@toptal.com>
2022-07-18 12:18:39 +0200

    Fix e2e tests

--- babel_.config.js
+++ babel_.config.js
@@ -1,6 +1,13 @@
-const shouldInstrumentCode = 'E2E_COVERAGE' in process.env
+// Once istanbul plugin support is available for the swc, we need to remove this file and update build script.
+// Reference: https://github.com/vercel/next.js/discussions/30174
+
+const plugins = []
+
+if (process.env.E2E_COVERAGE) {
+  plugins.push('babel-plugin-istanbul')
+}
 
 module.exports = {
   presets: ['next/babel'],
-  plugins: shouldInstrumentCode ? ['istanbul'] : []
+  plugins
 }
